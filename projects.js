67ed6f6887e8010a3fd2b747bf34d0a9cfbfa732
folicks <fnajera@ucsd.edu>
2025-02-01 13:17:59 -0800

    backtrack projects.js

--- projects.js
+++ projects.js
@@ -1,18 +1,18 @@
-import { fetchJSON, renderProjects } from '../global.js';
+// import { fetchJSON, renderProjects } from '../global.js';
 
-(async function () {
-    const projects = await fetchJSON('../lib/projects.json');
-    const projectsContainer = document.querySelector('.projects');
+// (async function () {
+//     const projects = await fetchJSON('../lib/projects.json');
+//     const projectsContainer = document.querySelector('.projects');
 
-    if (projects && projects.length > 0) {
-        renderProjects(projects, projectsContainer, 'h2');
+//     if (projects && projects.length > 0) {
+//         renderProjects(projects, projectsContainer, 'h2');
         
-        // Count and update project title
-        const projectsTitle = document.querySelector('.projects-title');
-        if (projectsTitle) {
-            projectsTitle.textContent = `Projects (${projects.length})`;
-        }
-    } else {
-        projectsContainer.innerHTML = '<p>No projects found.</p>';
-    }
-})();
+//         // Count and update project title
+//         const projectsTitle = document.querySelector('.projects-title');
+//         if (projectsTitle) {
+//             projectsTitle.textContent = `Projects (${projects.length})`;
+//         }
+//     } else {
+//         projectsContainer.innerHTML = '<p>No projects found.</p>';
+//     }
+// })();
