97b630397954b01ceab709a49e39858a0ef803f6
folicks <fnajera@ucsd.edu>
2025-02-03 14:40:26 -0800

    made progress then lost it

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,16 @@
+
+
+
 import { fetchJSON, renderProjects } from '../globalStep3.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+
+
+
+let data = [1, 2];
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arcData = [];
+let total = 0;
+let angle = 0;
 
 (async function () {
     const projects = await fetchJSON('../lib/project.json');
@@ -16,3 +28,43 @@ import { fetchJSON, renderProjects } from '../globalStep3.js';
         projectsContainer.innerHTML = '<p>No projects found.</p>';
     }
 })();
+
+
+// let arc = arcGenerator({
+//     startAngle: 0,
+//     endAngle: 2 * Math.PI,
+// });
+
+// d3.select('svg').append('path').attr('d', arc).attr('fill', 'blue');
+
+
+for (let d of data) {
+    total += d;
+}
+
+
+
+for (let d of data) {
+    let endAngle = angle + (d / total) * 2 * Math.PI;
+    arcData.push({ startAngle: angle, endAngle });
+    angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach(arc => {
+    g.append("path")
+      .attr('d', arc)
+      .attr('fill',"blue");
+
+})
+
+
+let colors = ['gold', 'purple'];
+
+// arcs.forEach((arc, idx) => {
+//     d3.select('svg g')
+//       .append('path')
+//       .attr('d', arc)
+//       .attr('fill', colors[idx]);
+// });
