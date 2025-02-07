6772e08885b2dc211f603e336bd493f7157832b9
folicks <fnajera@ucsd.edu>
2025-02-06 23:43:47 -0800

    step 2.1 done where are the labels in html?

--- projects/projects.js
+++ projects/projects.js
@@ -18,9 +18,18 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
     }
 })();
 
-let data = [1, 2];
+// let data = [1, 2, 3, 4, 5, 5];
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+];
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let sliceGenerator = d3.pie();
+// let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
@@ -31,13 +40,14 @@ let g = svg.append('g')
     .attr('transform', 'translate(0, 0)');
 
 
-let colors = ['gold', 'purple'];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+// RQ :  why does it need to be an () vs a []
 
 arcs.forEach((arc, idx) => {
     d3.select('svg g')
       .append('path')
       .attr('d', arc)
-      .attr('fill', colors[idx]);
+      .attr('fill', colors(idx));
 });
 
 
