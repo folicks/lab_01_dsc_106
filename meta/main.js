c6d5adf9b624108d61ad5bfb588a6a82d7a55569
folicks <fnajera@ucsd.edu>
2025-02-12 10:40:36 -0800

    i think i might be missing the lines array

--- meta/main.js
+++ meta/main.js
@@ -3,6 +3,7 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 
 
 let data = [];
+let commits = [];
 
 async function loadData() {
   data = await d3.csv('./loc.csv', (row) => ({
@@ -17,14 +18,37 @@ async function loadData() {
   // this works btw and actually reads
 }
 
+
+
+function processCommits() {
+  commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+
+      // We can use object destructuring to get these properties
+      let { author, date, time, timezone, datetime } = first;
+
+      return {
+        id: commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        // What other properties might be useful?
+      };
+    });
+}
+
+
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
+  processCommits();
+  console.log(commits);
 });
 
+// commits = d3.groups(data, (d) => d.commit);
+// console.log(commits);
+// // this doesn't work, it returns an empty array
 
-
-// i will do no such the more clutter the better
-
-
-let commits = d3.groups(data, (d) => d.commit);
-console.log(commits);
